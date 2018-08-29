e512108bb168763dcb30bf3552fafe380e48f715
TerrencePai <26332454252@qq.com>
2018-08-29 18:41:26 +0800

    啊

--- JavaScript文件/message.js
+++ JavaScript文件/message.js
@@ -1,5 +1,5 @@
-function success(){alert("收到了呢！")}
-		function load(){
+function success(){alert("收到了呢！");}
+	/*	function load(){
 		var xhr;
 if (window.XMLHttpRequest)
   {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -19,5 +19,4 @@ xhr.onreadystatechange=function()
 xhr.open("post","/ajax/demo_get.asp",true);
 xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 xhr.send("fname=Bill&lname=Gates");
-
-}
+}*/
