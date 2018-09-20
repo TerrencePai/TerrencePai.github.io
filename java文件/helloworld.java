6eff027b5e7a808a6f719fc6eafe350d946aedbd
TerrencePai <26332454252@qq.com>
2018-09-20 23:40:00 +0800

    去

--- java文件/helloworld.java
+++ java文件/helloworld.java
@@ -1,5 +1,7 @@
 public class helloworld{
 	public static void main(String[] args){
-	System.out.println("hello æˆ‘æ—¥");
+	System.out.println("hell   WW  orld");
+	System.out.println("wjibaiawenha  dsaaha");
+	System.out.println("°×°¢ÎÄÈöÀ­");
 	} 
-}
+	} 
